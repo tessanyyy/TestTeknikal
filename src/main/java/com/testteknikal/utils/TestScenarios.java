01d7b7d0e87f419a0ca0ef9253471e7b121c0fa0
IT-DIKA <ryoharyo54@gmail.com>
2023-08-21 10:16:10 +0700

    first

--- src/main/java/com/testteknikal/utils/TestScenarios.java
+++ src/main/java/com/testteknikal/utils/TestScenarios.java
@@ -1,15 +1,8 @@
 package com.testteknikal.utils;
 
 public enum TestScenarios {
-    //Scenario Modul Login Page
-    T1("Check access login page with invalid url"),
-    T2("Check access login page with invalid url"),
-    T3("Check Login is Failed with invalid password"),
-    T4("Check Login is Falied with invalid username"),
-    T5("Check Login is Falied with null password"),
-    T6("Check Login is Failed with null username"),
-    T7("Check Login is falied with null username and password"),
-    T8("Check Login is success with valid credentials"),
+    //Scenario Booking
+    T1("Booking Validation"),
 
 
     private String testCaseName;
